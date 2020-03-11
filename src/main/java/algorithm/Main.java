7b5d572fd965b956815f77526e291dc88b558436
jvan <2424895931@qq.com>
2020-03-11 21:23:01 +0800

    等会回滚

--- src/main/java/algorithm/Main.java
+++ src/main/java/algorithm/Main.java
@@ -11,6 +11,8 @@ public class Main {
 //        System.out.println(getResult(100));
 //        System.out.println(getResult1(3, 2));
 
+
+
         int[] arr={1,2,5,2};
 
         System.out.println(findRepeatNumber(arr));
